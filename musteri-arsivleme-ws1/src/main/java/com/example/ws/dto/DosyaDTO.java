74b512fc9b6a30e2efd680cee8a927a0102082ef
SimgeAcimis <simge.acimis@gmail.com>
2021-11-19 09:03:46 +0300

    Add files via upload

---
+++ musteri-arsivleme-ws1/src/main/java/com/example/ws/dto/DosyaDTO.java
@@ -0,0 +1,30 @@
+package com.example.ws.dto;
+
+import com.example.ws.ByteArrayDeSerializer;
+import com.example.ws.ByteArraySerializer;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+import java.io.Serializable;
+
+@Data
+public class DosyaDTO implements Serializable {
+
+    private static final long serialVersionUID = 1871409791412322308L;
+
+    @JsonProperty("dosya")
+    @JsonSerialize(using = ByteArraySerializer.class)
+    @JsonDeserialize(using = ByteArrayDeSerializer.class)
+    private byte[] dosya;
+
+    @JsonProperty("dosyaAdi")
+    private String dosyaAdi;
+
+    @JsonProperty("dosya_id")
+    private Long dosyaId;
+
+    @JsonProperty("musteri_id")
+    private Long musteriId;
+
+}
